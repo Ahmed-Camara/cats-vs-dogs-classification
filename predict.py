e6db9c3c4c37c65117068fa84cb8b97150f49371
Ahmed-Camara <camarahmed123@gmail.com>
2022-04-05 01:11:41 +0600

    cats dogs classifier

---
+++ predict.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
+import numpy as np
+from keras.models import load_model
+model = load_model('model/cats_dogs.h5')
+#dictionary to label all traffic signs class.
+classes = { 
+    0:'Cat',
+    1:'Dog',
+}
+#initialise GUI
+top=tk.Tk()
+top.geometry('800x600')
+top.configure(background='white')
+label=Label(top,background='white', font=('arial',15,'bold'))
+sign_image = Label(top)
+def classify(file_path):
+    global label_packed
+    image = Image.open(file_path)
+    image = image.resize((224,224))
+    img = np.array(image)
+    img = img.reshape(1,224, 224, 3)
+    
+    pred = model.predict(img)
+    sign = classes[int((pred>0.5)*1)]
+    print(sign)
+    label.configure(foreground='#011638', text=sign) 
+def show_classify_button(file_path):
+    classify_b=Button(top,text="Classify Image",
+   command=lambda: classify(file_path),
+   padx=10,pady=5)
+    classify_b.configure(background='#364156', foreground='white',
+font=('arial',10,'bold'))
+    classify_b.place(relx=0.79,rely=0.46)
+def upload_image():
+    try:
+        file_path=filedialog.askopenfilename()
+        uploaded=Image.open(file_path)
+        uploaded.thumbnail(((top.winfo_width()/2.25),
+    (top.winfo_height()/2.25)))
+        im=ImageTk.PhotoImage(uploaded)
+        sign_image.configure(image=im)
+        sign_image.image=im
+        label.configure(text='')
+        show_classify_button(file_path)
+    except:
+        pass
+upload=Button(top,text="Upload an image",command=upload_image,padx=10,pady=5)
+upload.configure(background='gray', foreground='white',font=('arial',10,'bold'))
+upload.pack(side=BOTTOM,pady=50)
+sign_image.pack(side=BOTTOM,expand=True)
+label.pack(side=BOTTOM,expand=True)
+heading = Label(top, text="Prediction",pady=20, font=('arial',20,'bold'))
+heading.configure(background='white',foreground='#364156')
+heading.pack()
+top.mainloop()
